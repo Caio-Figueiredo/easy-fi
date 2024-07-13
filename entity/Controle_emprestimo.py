7224b81e226914e12d77a6f4b0e6aa1837ce27fa
Vicente-Souza-Stramantino <vicente_stra@outlook.com>
2024-07-13 01:39:33 -0300

    Adiciona classes Sub_movimentacao e Controle_emprestimo

---
+++ entity/Controle_emprestimo.py
@@ -0,0 +1,17 @@
+class Controle_emprestimo:
+
+    def __init__(self, id_sub_mov, id_devolvedor):
+        self.id_devolvedor = id_devolvedor
+        self.id_sub_mov = id_sub_mov
+    
+    def set_id_devolvedor(self, id_devolvedor)
+        self.id_devolvedor = id_devolvedor
+    
+    def set_id_sub_mov(self, id_sub_mov):
+        self.id_sub_mov = id_sub_mov
+    
+    def get_id_sub_mov(self):
+        return self.id_devolvedor
+    
+    def get_id_sub_mov(self):
+        return self.id_sub_mov
---
+++ entity/Sub_movimentacao.py
@@ -0,0 +1,43 @@
+class Sub_movimentacao:
+    def __init__(self, id, data_prev_sub_movimentacao, data_real_sub_movimentacao, valor_sub_movimentacao, id_forma_pgto, id_movimentacao):
+        self.id = id
+        self.data_prev_sub_movimentacao = data_prev_sub_movimentacao
+        self.data_real_sub_movimentacao = data_real_sub_movimentacao
+        self.valor_sub_movimentacao = valor_sub_movimentacao
+        self.id_forma_pgto = id_forma_pgto
+        self.id_movimentacao =  id_movimentacao
+
+
+    #GET
+    def get_id(self):
+        return self.id
+
+    def get_data_prev_sub_movimentacao(self):
+        return self.data_prev_sub_movimentacao
+
+    def get_data_real_sub_movimentacao(self):
+        return self.data_real_sub_movimentacao
+    
+    def get_id_forma_pgto(self):
+        return self.id_forma_pgto
+    
+    def get_id_movimentacao(self):
+        return self.id_movimentacao
+
+
+
+    #SET
+    def set_id(self, id):
+        self.id = id
+
+    def set_data_prev_sub_movimentacao(self, data_prev_sub_movimentacao):
+        self.data_prev_sub_movimentacao = data_prev_sub_movimentacao
+
+    def set_data_real_sub_movimentacao(self, data_real_sub_movimentacao):
+        self.data_real_sub_movimentacao = data_real_sub_movimentacao
+
+    def set_valor_sub_movimentacao(self, valor_sub_movimentacao):
+        self.valor_sub_movimentacao = valor_sub_movimentacao
+
+    def set_id_movimentacao(self, id_movimentacao):
+        self.id_movimentacao = id_movimentacao
